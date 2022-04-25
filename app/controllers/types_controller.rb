d7feac90f638930c6b75510254a82bdb242e70c5
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-04-26 03:53:57 +0600

    Create types views and controller

---
+++ app/controllers/types_controller.rb
@@ -0,0 +1,10 @@
+class TypesController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  get '/types', to: 'types#index', as: 'user_root'
+  get '/types', to: 'types#index', as: 'user'
+
   resources :splash_screen, only: [:index]
+  resources :types, only: [:index, :new, :create]
   # Defines the root path route ("/")
   root "splash_screen#index"
 end
