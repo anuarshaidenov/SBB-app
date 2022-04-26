82c88be3413ab2ae2545aee70dba89d44eb9a9f0
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-04-26 17:56:18 +0600

    Style types#new page and add logic

--- app/controllers/types_controller.rb
+++ app/controllers/types_controller.rb
@@ -5,7 +5,23 @@ class TypesController < ApplicationController
     @types = Type.includes(:spendings).all
   end
 
-  def new; end
+  def new
+    @type = Type.new
+  end
 
-  def create; end
+  def create
+    type = Type.new(type_params)
+
+    if type.save
+      redirect_to root_path, notice: 'Type was successfully created!'
+    else
+      redirect_to new_type_path, alert: 'Type could not be created!'
+    end
+  end
+
+  private
+
+  def type_params
+    params.require(:type).permit(:icon, :name, :user_id)
+  end
 end
