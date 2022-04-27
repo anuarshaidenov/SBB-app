ad50030aa27823c6cd1a7d49ed9e1c520c566e7a
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-04-28 02:24:23 +0600

    Add mobile menu

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,17 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+import '@hotwired/turbo-rails';
+import 'controllers';
+window.addEventListener('load', () => {
+  const hamburgerBtn = document.getElementById('hamburger-btn');
+  const menu = document.getElementById('mobile-menu');
+  const backdrop = document.getElementById('backdrop');
+  hamburgerBtn.addEventListener('click', () => {
+    menu.classList.remove('-translate-x-full');
+    backdrop.classList.remove('hidden');
+  });
+
+  backdrop.addEventListener('click', () => {
+    menu.classList.add('-translate-x-full');
+    backdrop.classList.add('hidden');
+  });
+});
