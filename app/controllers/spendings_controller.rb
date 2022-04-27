a97633660fc79b37cec3b57975859cd897f315d4
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-04-27 14:03:45 +0600

    Add spendings views and logic

--- app/controllers/spendings_controller.rb
+++ app/controllers/spendings_controller.rb
@@ -5,7 +5,24 @@ class SpendingsController < ApplicationController
     @spendings = Type.includes(:spendings).find(params[:type_id]).spendings
   end
 
-  def new; end
+  def new
+    @spending = Spending.new
+    @types = Type.all
+  end
 
-  def create; end
+  def create
+    spending = Spending.new(spending_params)
+
+    if spending.save
+      redirect_to root_path, notice: 'Spending was successfully created.'
+    else
+      redirect_to new_type_spending_path, alert: 'Spending was not created!'
+    end
+  end
+
+  private
+
+  def spending_params
+    params.require(:spending).permit(:author_id, :name, :amount, :type_id)
+  end
 end
