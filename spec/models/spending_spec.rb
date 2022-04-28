3799fce61195e5d337a40f08052a684092c167a1
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-04-28 13:48:10 +0600

    Add type model specs

--- spec/models/spending_spec.rb
+++ spec/models/spending_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Spending, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Spending model" do
+    before(:each) do
+      @user = User.create name: "Test", email: "text@example.com", password: "123456"
+      @type = Type.create name: 'Test type', icon: 'test.png', user: @user
+      @spending = Spending.create name: "Test spending", amount: 200, author: @user, type: @type
+    end
+
+    it "validates name" do
+      @spending.name = ""
+
+      expect(@spending).to_not be_valid  
+    end
+
+    it "validates amount" do
+      @spending.amount = ''
+
+      expect(@spending).to_not be_valid
+    end 
+
+    it "has a correct author" do
+      expect(@spending.author).to eq @user
+    end
+
+    it "has a correct type" do
+      expect(@spending.type).to eq @type
+    end
+  end
 end
