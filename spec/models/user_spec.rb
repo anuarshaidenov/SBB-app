fe662884b218785b6eba0bf9fd3e1c43727b23d2
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-04-28 13:33:29 +0600

    Add user model specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "User model" do
+    before (:each) do
+      @user = User.create name: "Test", email: "text@example.com", password: "123456"
+    end
+
+    it "validates name" do
+      @user.name = ''
+      expect(@user).to_not be_valid
+    end
+  end
 end
